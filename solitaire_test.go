f87ea5bd1f811592a5fc17c4c5657422d39736c5
Tijmen van der Burgt <tvdburgt@gmail.com>
2014-08-03 20:29:10 +0200

    use anonymous structs for test vectors

--- solitaire_test.go
+++ solitaire_test.go
@@ -3,61 +3,64 @@ package solitaire
 import (
 	"bytes"
 	"fmt"
-	"testing"
 	"regexp"
+	"testing"
 )
 
 var invalidChars = regexp.MustCompile(`[^a-zA-Z]`)
 
+// Test vectors
+var testVectors = []struct {
+	pt  string // Plaintext
+	ct  string // Ciphertext
+	key string // Key (passphrase)
+}{
+	// Default tests (Source: https://www.schneier.com/code/sol-test.txt)
+	{"AAAAAAAAAAAAAAA", "EXKYI ZSGEH UNTIQ", ""},
+	{"AAAAAAAAAAAAAAA", "XYIUQ BMHKK JBEGY", "f"},
+	{"AAAAAAAAAAAAAAA", "TUJYM BERLG XNDIW", "fo"},
+	{"AAAAAAAAAAAAAAA", "ITHZU JIWGR FARMW", "foo"},
+	{"AAAAAAAAAAAAAAA", "XODAL GSCUL IQNSC", "a"},
+	{"AAAAAAAAAAAAAAA", "OHGWM XXCAI MCIQP", "aa"},
+	{"AAAAAAAAAAAAAAA", "DCSQY HBQZN GDRUT", "aaa"},
+	{"AAAAAAAAAAAAAAA", "XQEEM OITLZ VDSQS", "b"},
+	{"AAAAAAAAAAAAAAA", "QNGRK QIHCL GWSCE", "bc"},
+	{"AAAAAAAAAAAAAAA", "FMUBY BMAXH NQXCJ", "bcd"},
+	{"AAAAAAAAAAAAAAAAAAAAAAAAA", "SUGSR SXSWQ RMXOH IPBFP XARYQ", "cryptonomicon"},
+	{"SOLITAIRE", "KIRAK SFJAN", "cryptonomicon"},
 
-// Test vectors. Source: https://www.schneier.com/code/sol-test.txt
-// Format: [[plaintext, ciphertext, key], ...]
-var testVectors = [][]string{
-	[]string{"AAAAAAAAAAAAAAA", "EXKYI ZSGEH UNTIQ", ""},
-	[]string{"AAAAAAAAAAAAAAA", "XYIUQ BMHKK JBEGY", "f"},
-	[]string{"AAAAAAAAAAAAAAA", "TUJYM BERLG XNDIW", "fo"},
-	[]string{"AAAAAAAAAAAAAAA", "ITHZU JIWGR FARMW", "foo"},
-	[]string{"AAAAAAAAAAAAAAA", "XODAL GSCUL IQNSC", "a"},
-	[]string{"AAAAAAAAAAAAAAA", "OHGWM XXCAI MCIQP", "aa"},
-	[]string{"AAAAAAAAAAAAAAA", "DCSQY HBQZN GDRUT", "aaa"},
-	[]string{"AAAAAAAAAAAAAAA", "XQEEM OITLZ VDSQS", "b"},
-	[]string{"AAAAAAAAAAAAAAA", "QNGRK QIHCL GWSCE", "bc"},
-	[]string{"AAAAAAAAAAAAAAA", "FMUBY BMAXH NQXCJ", "bcd"},
-	[]string{"AAAAAAAAAAAAAAAAAAAAAAAAA", "SUGSR SXSWQ RMXOH IPBFP XARYQ", "cryptonomicon"},
-	[]string{"SOLITAIRE", "KIRAK SFJAN", "cryptonomicon"},
+	// Distribution test
+	{"ABCDEFGHIJKLMNOPQRSTUVWXYZ", "EYMBM EYNMQ EYFVE KLJQD UUTWG CMTPJ", ""},
+	{"ABCDEFGHIJKLMNOPQRSTUVWXYZ", "EHZXE QOZQX HHKDG WKESR DVHAV LMXVZ", "foobarbaz"},
 
-	// // Distribution test
-	[]string{"ABCDEFGHIJKLMNOPQRSTUVWXYZ", "EYMBM EYNMQ EYFVE KLJQD UUTWG CMTPJ", ""},
-	[]string{"ABCDEFGHIJKLMNOPQRSTUVWXYZ", "EHZXE QOZQX HHKDG WKESR DVHAV LMXVZ", "foobarbaz"},
-
-	// // Padding test
-	[]string{"A", "EUHVF", ""},
-	[]string{"??A?", "EUHVF", ""},
-	[]string{"A", "IQEWR", "foo"},
+	// Padding test
+	{"A", "EUHVF", ""},
+	{"??A?", "EUHVF", ""},
+	{"A", "IQEWR", "foo"},
 }
 
 func TestEncrypt(t *testing.T) {
 	for i, v := range testVectors {
-		pt, ct, key := v[0], v[1], v[2]
-		output := Encrypt([]byte(pt), []byte(key))
-		ct = invalidChars.ReplaceAllString(ct, "")
+		// pt, ct, key := v[0], v[1], v[2]
+		ct := invalidChars.ReplaceAllString(v.ct, "")
+		output := Encrypt([]byte(v.pt), []byte(v.key))
 
 		if !bytes.Equal(output, []byte(ct)) {
 			t.Errorf("#%d: Encrypt(%q, %q) returns %q (expecting %q)",
-			i+1, pt, key, output, ct)
+				i+1, v.pt, v.key, output, ct)
 		}
 	}
 }
 
 func TestDecrypt(t *testing.T) {
 	for i, v := range testVectors {
-		pt, ct, key := v[0], v[1], v[2]
-		pt = invalidChars.ReplaceAllString(pt, "")
-		output := Decrypt([]byte(ct), []byte(key))
+		// pt, ct, key := v[0], v[1], v[2]
+		pt := invalidChars.ReplaceAllString(v.pt, "")
+		output := Decrypt([]byte(v.ct), []byte(v.key))
 
-		if len(output) % 5 > 0 || !bytes.HasPrefix(output, []byte(pt)) {
+		if len(output)%5 > 0 || !bytes.HasPrefix(output, []byte(pt)) {
 			t.Errorf("#%d: Decrypt(%q, %q) returns %q (expecting %q)",
-			i+1, ct, key, output, pt)
+				i+1, v.ct, v.key, output, pt)
 		}
 	}
 }
